dfb4eb2564308799bc9f2c2b71f1e499bbc0915a
Fernando Ortiz <fortiz2k@gmail.com>
2016-07-05 14:50:54 -0500

    add slrngen-net port

---
+++ src/slrngen-net/Library.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Hellosaint.SLRN
+{
+    public class SLRNGenerator
+    {
+        private static Random rnd = new Random();
+        private static int min    = 10000000;
+        private static int max    = 99999999;
+
+        public static String CreateSLRN() 
+        {
+            int eigth = rnd.Next(min, max); 
+            int ninth = CalculateChecksumDigit("" + eigth);
+            return Convert.ToString(eigth) + Convert.ToString(ninth);
+        }
+        
+        public static bool VerifySLRN(String code)
+        {
+            String result      = code.Replace("-", "");
+            String eigthString = result.Substring(0,result.Length-1);
+            int ninth          = CalculateChecksumDigit(eigthString);
+            return (eigthString + ninth).Equals(result);
+        }
+
+        public static int CalculateChecksumDigit(String code)
+        {
+            String prefixAndCode = "2672" + code;
+            String codeWithoutVd = prefixAndCode.Substring(0, 12);
+            int e = SumEven(codeWithoutVd);
+            int o = SumOdd(codeWithoutVd);
+            int me = o * 3;
+            int s = me + e;
+            int dv = GetEanVd(s);
+            return dv;
+        }
+
+        private static int GetEanVd(int s) 
+        {
+            return 10 - (s % 10);
+        }
+
+        private static int SumEven(String code) 
+        {
+            int sum = 0;
+            for (int i = 0; i < code.Length; i++) 
+            {
+                if (IsEven(i)) 
+                {
+                    sum += (int)Char.GetNumericValue(code[i]);
+                }
+            }
+            return sum;
+        }
+
+        private static int SumOdd(String code) 
+        {
+            int sum = 0;
+            for (int i = 0; i < code.Length; i++) {
+                if (!IsEven(i)) 
+                {
+                    sum += (int)Char.GetNumericValue(code[i]);
+                }
+            }
+            return sum;
+        }
+
+        private static bool IsEven(int i) 
+        {
+            return i % 2 == 0;
+        }
+
+    }
+}
---
+++ test/test-slrngen-net/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+using static Hellosaint.SLRN.SLRNGenerator;
+
+namespace Hellosaint.SLRN.Tests
+{
+    public class SLRNGeneratorTests
+    {
+        [Fact]
+        public void TestCreateSLRN() 
+        {
+            String slrn = CreateSLRN();
+            bool valid = VerifySLRN(slrn);
+            Assert.True(valid);
+            Console.WriteLine("Create: " + slrn + " is valid: " + valid);
+        }
+
+        [Fact]
+        public void TestVerifySLRN() 
+        {
+            bool valid = VerifySLRN("123-456-781");
+            Assert.True(valid);
+            Console.WriteLine("Verify: 123-456-781 is valid: " + valid);
+        }
+    }
+}
